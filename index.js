09a5b72c724a437706b255b64959393dfd2f2e67
DianaTuei <memotuei@gmail.com>
2024-01-06 11:14:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    let p = document.getElementById("text")
+    p.remove()
+    let body = document.querySelector("body")
+    body.innerHTML = '<p id="text">This is really cool!</p>'
+
+})
